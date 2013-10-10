3ec6dbe02def32a26753fb939217a7dfa04f4de1
Chris Kelner <chris.kelner@gmail.com>
2013-10-09 22:47:09 -0400

    load app data from db on load
    ignore node_modules
    remove some logging, clean it up!
    broke down some functions into smaller bites
    removed the package.json from inside the app folder

--- app/public/javascripts/main.js
+++ app/public/javascripts/main.js
@@ -4,85 +4,97 @@ app.map = {};
 app.map.openMarkers = [];
 app.socket = io.connect(window.location.hostname);
 app.init = function() {
-  app.processTweetData();
   app.fillMapHeight();
   kelner_googleMaps.initialize();
+  app.loadTweetDataFromDB();
+  app.setupTweetStreamSocket();
 }
 // sorry ass JS hack to set the map height to half the window size... 
 // 50% in css doesn't seem to work
 app.fillMapHeight = function() {
   $("#container").css("height", $(window).height() - $(window).height() / 2);
 }
-app.processTweetData = function() {
+app.loadTweetDataFromDB = function() {
+  app.socket.on('firstShow', function (data) {
+    var size = data.tweets.length
+    for(var i=0;i<size;i++){
+      app.processTweetData(data.tweets[i]);
+    }
+  });
+}
+app.setupTweetStreamSocket = function() {
   app.socket.on('data', function(tweet) {
-    // lat lon location of the tweet
-    var tweetLatLng = null;
-    var htmlStr =
-      "<div class='tweet'>" +
-        "<span class='tweet_text'>" +
-          "<strong>tweet: </strong> " + tweet.text +
-          "<br>" +
-          "<strong>tweeted on:</strong> " + tweet.created_at;
-    // this is the preferred lat/lon object
-    if( tweet.coordinates ){
-      htmlStr +=
+    app.processTweetData(tweet);
+  });
+}
+app.processTweetData = function(tweet) {
+  // lat lon location of the tweet
+  var tweetLatLng = null;
+  var htmlStr =
+    "<div class='tweet'>" +
+      "<span class='tweet_text'>" +
+        "<strong>tweet: </strong> " + tweet.text +
         "<br>" +
-        "<strong>tweeted from geocode:</strong> " +
-          tweet.coordinates.coordinates[0] + ", " + tweet.coordinates.coordinates[1];
-      tweetLatLng = new google.maps.LatLng(
-        tweet.coordinates.coordinates[1],
-        tweet.coordinates.coordinates[0]
-      );
+        "<strong>tweeted on:</strong> " + tweet.created_at;
+  // this is the preferred lat/lon object
+  if( tweet.coordinates ){
+    htmlStr +=
+      "<br>" +
+      "<strong>tweeted from geocode:</strong> " +
+        tweet.coordinates.coordinates[0] + ", " + tweet.coordinates.coordinates[1];
+    tweetLatLng = new google.maps.LatLng(
+      tweet.coordinates.coordinates[1],
+      tweet.coordinates.coordinates[0]
+    );
+  }
+  // human readable location, ex: Atlanta, Georgia United States
+  if( tweet.place ) {
+    htmlStr +=
+      "<br>" +
+      "<strong>tweeted from place:</strong> " +
+      tweet.place.full_name + ", " + tweet.place.country;
+    // only need to try to get the lat/lon if we don't have it already
+    if( !tweet.coordinates ){
+      app.reverseGeoCodeAddress( tweet.place.full_name + ", " + tweet.place.country, htmlStr );
     }
-    // human readable location, ex: Atlanta, Georgia United States
-    if( tweet.place ) {
-      htmlStr +=
-        "<br>" +
-        "<strong>tweeted from place:</strong> " +
-        tweet.place.full_name + ", " + tweet.place.country;
-      // only need to try to get the lat/lon if we don't have it already
-      if( !tweet.coordinates ){
-        app.reverseGeoCodeAddress( tweet.place.full_name + ", " + tweet.place.country, htmlStr );
-      }
-    }
-    htmlStr += "</span>";
-    if( tweet.entities ) {
-      // see if there is a link in there we can use
-      //TODO: go back and try to show this link if it is an image?
-      if( tweet.entities.urls && tweet.entities.urls.length > 0 ) {
-        if( tweet.entities.urls[0].expanded_url ) {
-          htmlStr +=
-          "<br>" +
-          "<span class='tweet_link'>" +
-            "<a href='" +tweet.entities.urls[0].expanded_url + "'>" +
-              tweet.entities.urls[0].expanded_url +
-            "</a>" +
-          "</span>" +
-          "<br><br>";
-        }
-      }
-      // is there a direct twitter upload photo we can display?
-      if( tweet.entities.media != null && tweet.entities.media.length > 0 ) {
+  }
+  htmlStr += "</span>";
+  if( tweet.entities ) {
+    // see if there is a link in there we can use
+    //TODO: go back and try to show this link if it is an image?
+    if( tweet.entities.urls && tweet.entities.urls.length > 0 ) {
+      if( tweet.entities.urls[0].expanded_url ) {
         htmlStr +=
-          "<br>" +
-          "<span class='tweet_img'>" +
-            "<img src='" + tweet.entities.media[0].media_url + "'/>" +
-          "</span>" +
-          "</div>" +
-          "<br><br>";
-      } else {
-        htmlStr += "</div><br><br>";
+        "<br>" +
+        "<span class='tweet_link'>" +
+          "<a href='" +tweet.entities.urls[0].expanded_url + "'>" +
+            tweet.entities.urls[0].expanded_url +
+          "</a>" +
+        "</span>" +
+        "<br><br>";
       }
+    }
+    // is there a direct twitter upload photo we can display?
+    if( tweet.entities.media != null && tweet.entities.media.length > 0 ) {
+      htmlStr +=
+        "<br>" +
+        "<span class='tweet_img'>" +
+          "<img src='" + tweet.entities.media[0].media_url + "'/>" +
+        "</span>" +
+        "</div>" +
+        "<br><br>";
     } else {
       htmlStr += "</div><br><br>";
     }
-    $("#tweet_map").prepend(htmlStr);
-    // this is only if coords were available, if we had to use 'place' then
-    // the callpack for reverse geocoding lookup will create the map marker
-    if( tweet.coordinates ){
-      app.createMapMarker( htmlStr, tweetLatLng )
-    }
-  });
+  } else {
+    htmlStr += "</div><br><br>";
+  }
+  $("#tweet_map").prepend(htmlStr);
+  // this is only if coords were available, if we had to use 'place' then
+  // the callpack for reverse geocoding lookup will create the map marker
+  if( tweet.coordinates ){
+    app.createMapMarker( htmlStr, tweetLatLng )
+  }
 }
 // do reverse lookup of street address (twitter seems to only provide City, State/Province, Country)
 app.reverseGeoCodeAddress = function( address, htmlStr ) {
