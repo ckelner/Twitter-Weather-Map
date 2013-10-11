430647b7c30e0d904399bca43e5da9e164234cc3
Adam Williams <kainolophobia@gmail.com>
2013-10-11 02:26:55 +0000

    added recent listing

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -8,6 +8,19 @@ exports.findById = function(req, res) {
 	});
 };
 
+exports.findRecent = function(req, res) {
+        var end = new Date();
+        var start = new Date();
+        start.setMinutes(start.getMinutes() - 15);
+	console.log(start);
+	console.log(end);
+	db.collection('posts', function(err, collection) {
+		collection.find({created_on: {$gte: start, $lt: end}}).toArray(function(err, items) {
+			res.send(items);
+		});
+	});
+};
+
 exports.findAll = function(req, res) {
 	db.collection('posts', function(err, collection) {
 		collection.find().toArray(function(err, items) {
